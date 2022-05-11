12480be1a274b32afb6a0b4298beb83cf5e1e8e5
Yongil Kim <yongil.k01@gmail.com>
2022-05-11 23:09:49 +0900

    first commit

--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -8,10 +8,59 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(items, date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = items;
+    this._date = date;
+  }
+
+  removeTodoItem = (todoId) => {
+    const targetTodoItemIndex = this._items.findIndex(
+      (todo) => todo.id === todoId
+    );
+    if (targetTodoItemIndex === - 1) return;
+    this._items.splice(targetTodoItemIndex, 1);
+  }
+
+  pushTodoItem = (todoItem) => {
+    this._items.push(todoItem);
+  }
+
+  isPrevItem = (todoId) => {
+    const targetTodoItemIndex = this._items.findIndex(
+      (todo) => todo.id === todoId
+    );
+    if (targetTodoItemIndex === - 1) return;
+
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreateAt(this._date);
+  _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreateAt(this._date);
+
+  _completedFilter = (todoItem) => todoItem._completed;
+  _notCompletedFilter = (todoItem) => !todoItem._completed;
+
+
+  get equalsDayItems() {
+    return this._items.filter(this._equalsDayFilter);
+  }
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._notCompletedFilter);
+  }
+
+  get notEqualsDayItems() {
+    return this._items.filter(this._notEqualsDayFilter);
   }
 }
 
